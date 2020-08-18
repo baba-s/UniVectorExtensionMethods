9750695ab406074e4bfcf66c772738e7cf30b1f2
baba_s <shotababa1206@gmail.com>
2020-08-18 16:22:29 +0900

    first commit

---
+++ Scripts/Vector2Ext.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+	/// <summary>
+	/// Vector2 型の拡張メソッドを管理するクラス
+	/// </summary>
+	public static class Vector2Ext
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 値を四捨五入して返します
+		/// </summary>
+		public static Vector2 Round( this Vector2 self )
+		{
+			return new Vector2
+			(
+				Mathf.Round( self.x ),
+				Mathf.Round( self.y )
+			);
+		}
+	}
+}
---
+++ Scripts/Vector3Ext.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+	/// <summary>
+	/// Vector3 型の拡張メソッドを管理するクラス
+	/// </summary>
+	public static class Vector3Ext
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 値を四捨五入して返します
+		/// </summary>
+		public static Vector3 Round( this Vector3 self )
+		{
+			return new Vector3
+			(
+				Mathf.Round( self.x ),
+				Mathf.Round( self.y ),
+				Mathf.Round( self.z )
+			);
+		}
+	}
+}
